b3d4029a1634cdd518b74bc025aa993da6a896e6
Naan Mohammed <naanahmed24@gmail.com>
2023-03-14 03:09:09 +0300

    Update index.js to fix linter errors

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const page = document.getElementById('translucent_page');
 const closeButton = document.getElementById('close_button');
 const lis = document.querySelectorAll('#translucent_page li');
 
-button.addEventListener('click', function () {
+button.addEventListener('click', function handleTranslucentPage(event) {
   event.preventDefault();
   page.style.display = 'block';
 });
 
-closeButton.addEventListener('click', function () {
+closeButton.addEventListener('click', function handleButtonClick() {
   page.style.display = 'none';
 });
 
 lis.forEach(li => {
-  li.addEventListener('click', function () {
+  li.addEventListener('click', function handleLiClick() {
     const sectionId = this.getAttribute('data-section');
     const section = document.getElementById(sectionId);
     if (section) {
