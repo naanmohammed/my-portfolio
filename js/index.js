db93ff13b6eb6cad86733bdc2ff32ad5843c5320
naanmohammed <naanahmed24@gmail.com>
2023-05-15 19:11:54 +0300

    Update paths for js files

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('my_button');
+const page = document.getElementById('translucent_page');
+const closeButton = document.getElementById('close_button');
+const lis = document.querySelectorAll('#translucent_page li');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  page.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  page.style.display = 'none';
+});
+
+lis.forEach((li) => {
+  li.addEventListener('click', function handleLiClick() {
+    const sectionId = this.getAttribute('data-section');
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+    page.style.display = 'none';
+  });
+});
